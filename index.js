bc44217bf5131150c168c0fc72751151271b2363
angimenez <angimenez1996@gmail.com>
2020-10-01 19:02:05 -0300

    COMMIT

--- index.js
+++ index.js
@@ -95,8 +95,28 @@ function _nonIterableRest() {
   throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
 }
 
+function _templateObject6() {
+  var data = _taggedTemplateLiteral(["\n  width: 100%;\n  height: auto;\n  border-radius: 5px;\n  ", "\n"]);
+
+  _templateObject6 = function _templateObject6() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject5() {
+  var data = _taggedTemplateLiteral(["\n  display: flex;\n  flex-direction: column;\n  cursor: pointer;\n  width: 100%;\n  margin: 10px 0 10px 0;\n  border-radius: 5px;\n  background-color: whitesmoke;\n  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);\n  transition: all 1s;\n  ", "\n"]);
+
+  _templateObject5 = function _templateObject5() {
+    return data;
+  };
+
+  return data;
+}
+
 function _templateObject4() {
-  var data = _taggedTemplateLiteral(["\n  width: 100%;\n  height: auto;\n  border-radius: 5px;\n"]);
+  var data = _taggedTemplateLiteral(["\n  text-align: center;\n  color: black;\n"]);
 
   _templateObject4 = function _templateObject4() {
     return data;
@@ -106,7 +126,7 @@ function _templateObject4() {
 }
 
 function _templateObject3() {
-  var data = _taggedTemplateLiteral(["\n  display: flex;\n  flex-direction: column;\n  width: 100%;\n  margin: 10px 0 10px 0;\n  border-radius: 5px;\n  background-color: whitesmoke;\n  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);\n  transition: all 1s;\n  ", "\n  :hover {\n    transform: scale(1.02);\n  }\n"]);
+  var data = _taggedTemplateLiteral(["\n  position: absolute;\n  cursor: pointer;\n  outline: none;\n  right: 10px;\n  top: 10px;\n  width: 55px;\n  height: 20px;\n  border-radius: 50px;\n  font-size: 0.8em;\n  color: black;\n  border: none;\n  background: white;\n  @media (max-width: 600px) {\n    width: 45px;\n    height: 17px;\n    font-size: 0.5em;\n  }\n"]);
 
   _templateObject3 = function _templateObject3() {
     return data;
@@ -116,7 +136,7 @@ function _templateObject3() {
 }
 
 function _templateObject2() {
-  var data = _taggedTemplateLiteral(["\n  text-align: center;\n  color: black;\n"]);
+  var data = _taggedTemplateLiteral(["\n  position: fixed;\n  visibility: ", ";\n  top: 0;\n  left: 0;\n  bottom: 0;\n  right: 0;\n  background-color: black;\n  z-index: 1;\n  opacity: 0.5;\n"]);
 
   _templateObject2 = function _templateObject2() {
     return data;
@@ -126,7 +146,7 @@ function _templateObject2() {
 }
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  position: relative;\n  cursor: pointer;\n  outline: none;\n  right: 10px;\n  top: 10px;\n  width: 55px;\n  height: 20px;\n  border-radius: 50px;\n  font-size: 0.8em;\n  font-family: \"Montserrat\";\n  color: black;\n  border: none;\n  background: white;\n  @media (max-width: 600px) {\n    width: 45px;\n    height: 17px;\n    font-size: 0.5em;\n  }\n"]);
+  var data = _taggedTemplateLiteral(["\n  display: flex;\n  justify-content: center;\n  width: 100%;\n  background-color: black;\n"]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -144,28 +164,44 @@ var PictureBox = function PictureBox(_ref) {
       popup = _useState2[0],
       setPopup = _useState2[1];
 
-  var handleClick = function handleClick() {
-    setPopup(!popup);
+  var handleClose = function handleClose(e) {
+    e.stopPropagation();
+    setPopup(false);
   };
 
-  return /*#__PURE__*/React__default.createElement(PictureContainer, {
+  var handleOpen = function handleOpen(e) {
+    e.stopPropagation();
+    setPopup(true);
+  };
+
+  return /*#__PURE__*/React__default.createElement(React__default.Fragment, null, /*#__PURE__*/React__default.createElement(Hidden, {
+    visible: popup,
+    onClick: handleClose
+  }), /*#__PURE__*/React__default.createElement(PictureContainer, {
     popup: popup,
-    onClick: handleClick
-  }, /*#__PURE__*/React__default.createElement(Picture, {
+    onClick: handleOpen
+  }, /*#__PURE__*/React__default.createElement(BackgroundImage, null, /*#__PURE__*/React__default.createElement(Picture, {
     src: url,
-    alt: description
-  }), popup ? /*#__PURE__*/React__default.createElement(Exit, {
-    onClick: handleClick,
+    alt: description,
+    popup: popup
+  })), popup ? /*#__PURE__*/React__default.createElement(Exit, {
+    onClick: handleClose,
     visible: !popup
-  }, "Cerrar") : null, description ? /*#__PURE__*/React__default.createElement(Text, null, description) : null);
+  }, "Cerrar") : null, description ? /*#__PURE__*/React__default.createElement(Text, null, description) : null));
 };
 
-var Exit = styled.button(_templateObject());
-var Text = styled.p(_templateObject2());
-var PictureContainer = styled.div(_templateObject3(), function (props) {
-  return props.popup ? "\n      position: fixed;\n      top: 10px;\n      left: 5px;\n      right: 5px;\n      overflow: auto;\n      z-index:2;\n  " : "";
+var BackgroundImage = styled.div(_templateObject());
+var Hidden = styled.div(_templateObject2(), function (props) {
+  return props.visible ? "visible" : "hidden";
 });
-var Picture = styled.img(_templateObject4());
+var Exit = styled.button(_templateObject3());
+var Text = styled.p(_templateObject4());
+var PictureContainer = styled.div(_templateObject5(), function (props) {
+  return props.popup ? "\n      cursor: default;\n      position: fixed;\n      top: 10px;\n      left: 5px;\n      right: 5px;\n      overflow: auto;\n      z-index:2;\n      margin: 0;\n      max-height: 90vh;\n      align-items: center;\n      width: auto;\n  " : "\n        :hover {\n            transform: scale(1.02);\n        }";
+});
+var Picture = styled.img(_templateObject6(), function (props) {
+  return props.popup ? "\n        height: 80vh;\n        width: auto;\n        @media (max-width: 600px) {\n            width: 100%;\n            height: auto;\n        }\n        " : "";
+});
 
 function _templateObject2$1() {
   var data = _taggedTemplateLiteral(["\n  display: flex;\n  flex-direction: row;\n  justify-content: center;\n  width: 100%;\n"]);
--- src/PictureBox.js
+++ src/PictureBox.js
@@ -4,25 +4,54 @@ import styled from "styled-components";
 const PictureBox = ({ url, description }) => {
   const [popup, setPopup] = useState(false);
 
-  const handleClick = () => {
-    setPopup(!popup);
+  const handleClose = (e) => {
+    e.stopPropagation();
+    setPopup(false);
+  };
+  const handleOpen = (e) => {
+    e.stopPropagation();
+    setPopup(true);
   };
 
   return (
-    <PictureContainer popup={popup} onClick={handleClick}>
-      <Picture src={url} alt={description} />
-      {popup ? (
-        <Exit onClick={handleClick} visible={!popup}>
-          Cerrar
-        </Exit>
-      ) : null}
-      {description ? <Text>{description}</Text> : null}
-    </PictureContainer>
+    <>
+      <Hidden visible={popup} onClick={handleClose} />
+      <PictureContainer popup={popup} onClick={handleOpen}>
+        <BackgroundImage>
+          <Picture src={url} alt={description} popup={popup} />
+        </BackgroundImage>
+        {popup ? (
+          <Exit onClick={handleClose} visible={!popup}>
+            Cerrar
+          </Exit>
+        ) : null}
+        {description ? <Text>{description}</Text> : null}
+      </PictureContainer>
+    </>
   );
 };
 
+const BackgroundImage = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  background-color: black;
+`;
+
+const Hidden = styled.div`
+  position: fixed;
+  visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: black;
+  z-index: 1;
+  opacity: 0.5;
+`;
+
 const Exit = styled.button`
-  position: relative;
+  position: absolute;
   cursor: pointer;
   outline: none;
   right: 10px;
@@ -31,7 +60,6 @@ const Exit = styled.button`
   height: 20px;
   border-radius: 50px;
   font-size: 0.8em;
-  font-family: "Montserrat";
   color: black;
   border: none;
   background: white;
@@ -50,6 +78,7 @@ const Text = styled.p`
 const PictureContainer = styled.div`
   display: flex;
   flex-direction: column;
+  cursor: pointer;
   width: 100%;
   margin: 10px 0 10px 0;
   border-radius: 5px;
@@ -59,23 +88,39 @@ const PictureContainer = styled.div`
   ${(props) =>
     props.popup
       ? `
+      cursor: default;
       position: fixed;
       top: 10px;
       left: 5px;
       right: 5px;
       overflow: auto;
       z-index:2;
+      margin: 0;
+      max-height: 90vh;
+      align-items: center;
+      width: auto;
   `
-      : ""}
-  :hover {
-    transform: scale(1.02);
-  }
+      : `
+        :hover {
+            transform: scale(1.02);
+        }`}
 `;
 
 const Picture = styled.img`
   width: 100%;
   height: auto;
   border-radius: 5px;
+  ${(props) =>
+    props.popup
+      ? `
+        height: 80vh;
+        width: auto;
+        @media (max-width: 600px) {
+            width: 100%;
+            height: auto;
+        }
+        `
+      : ``}
 `;
 
 export default PictureBox;
